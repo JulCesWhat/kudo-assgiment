a6b291815eb7bc299ba66244c8cef99517c16225
Whatley, Julio (jw970q) <jw970q@att.com>
2021-03-01 23:21:35 -0500

    Adding main components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { LeaderboardComponent } from './views/leaderboard/leaderboard.component';
+import { NotfoundComponent } from './views/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: DashboardComponent,
+    },
+    {
+        path: 'leaderboard',
+        component: LeaderboardComponent,
+    },
+    {
+        path: '**',
+        component: NotfoundComponent,
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+
+import { ApiService } from './services/api.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeaderboardComponent } from './views/leaderboard/leaderboard.component';
+import { NotfoundComponent } from './views/notfound/notfound.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LeaderboardComponent,
+        NotfoundComponent,
+        DashboardComponent,
+        HeaderComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        StoreModule.forRoot({}, {}),
+        EffectsModule.forRoot([]),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+    ],
+    providers: [
+        ApiService
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
